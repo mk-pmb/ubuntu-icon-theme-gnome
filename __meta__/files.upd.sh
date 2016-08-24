991d9cdb113736ca37378041f3994aa26b899cc0
mk-pmb <mk-pmb@users.noreply.github.com>
2016-08-24 02:15:37 +0200

    meta data

---
+++ __meta__/files.upd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+function upd_flist () {
+  local SELFFILE="$(readlink -m "$BASH_SOURCE")"
+  local SELFPATH="$(dirname "$SELFFILE")"
+  cd "$SELFPATH"/.. || return $?
+  find [A-Za-z]*/ -name 'icon-theme.cache' -print -delete
+  EZGAL_LISTFN="${SELFFILE%.upd.sh}" ezgal3ry-update-file-lists gnome
+  return 0
+}
+
+upd_flist "$@"; exit $?
